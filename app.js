9b1b31c137e8e44b309578483f0228e09e629735
jaelomin <jaelomin@urbanbase.com>
2018-03-06 11:57:40 +0900

    express 에서 koa 로 전체 변경

--- app.js
+++ app.js
@@ -1,52 +1,23 @@
 require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 
-const index = require('./routes/index');
-const users = require('./routes/users');
+const Koa = require('koa');
+const Router = require('koa-router');
+
+const router = new Router()
+const app = new Koa();
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.mongo_URI)
-    .then(res => console.log('mongoDB connect successful'))
-    .catch(err => console.error(err));
-
-const app = express();
-
-// view engine setup
+    .then(res => console.log('mongoDB connect sucess!'))
+    .catch(err => console.log(err))
 
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(function* () {
+    this.body = 'hello world'
+})
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(3000, () => {
+    console.log('server running on localhost:3000')
+})
